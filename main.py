06764874ff4331dc33a522ddcb3633c370e975fe
Force-quit <miloulune1@gmail.com>
2023-11-23 18:36:41 -0500

    Skip works and Player is removed from active players when done

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ async def ping(ctx):
     await message.add_reaction("👌")
 
 @bot.slash_command()
+async def skip(ctx):
+    if players.get(ctx.guild):
+        await players[ctx.guild].skip(ctx)
+    else:
+        interraction = await ctx.respond("No music is playing wa de hell boi")
+        message = await interraction.original_response()
+        await message.add_reaction("🤨")
+
+@bot.slash_command()
 async def play(ctx, url):
     if is_connected(ctx):
         await players[ctx.guild].queue(ctx, url)
@@ -27,13 +36,16 @@ async def play(ctx, url):
     voice_client = await voice_channel.connect()
     
     #generate the Player instance that will handle cleanup with existing request data
-    players[ctx.guild] = Player(voice_client, command_channel, asyncio.get_running_loop())
+    players[ctx.guild] = Player(voice_client, command_channel, asyncio.get_running_loop(), ctx.guild, player_finished)
     await players[ctx.guild].play(url, interaction)
 
 #code that checks using the request context if the bot is connected to a Voice Channel in the same server(partial fix to multi-call issue)
 def is_connected(ctx):
     voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
     return voice_client and voice_client.is_connected()
+
+def player_finished(guild_id):
+    del players[guild_id]
 
 if __name__ == "__main__":
     bot.run(config["TOKEN"])
