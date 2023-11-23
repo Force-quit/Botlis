a0b92bb490b52fce8b948d1db74bbb181ddf28ec
Force-quit <miloulune1@gmail.com>
2023-11-23 13:07:44 -0500

    Almost there
    Co-authored-by: Elouan Hildgen-Poirier <CmD0@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import asyncio, discord, io
+import asyncio, discord
 from player import Player
 from dotenv import dotenv_values
-from pytube import YouTube
 
 config = dotenv_values(".env")
 
 bot = discord.Bot()
+players = {}
 
 @bot.slash_command(guild_ids=["1177073238346567760"])
 async def ping(ctx):
@@ -15,33 +15,19 @@ async def ping(ctx):
 
 @bot.slash_command()
 async def play(ctx, url):
-    if not is_connected(ctx):
-        #interact with the request to enable 15 minute interaction window
-        interaction = await ctx.respond("Loading this bitch up")
-        
-        #gather the data for the request
-        channel = ctx.author.voice.channel
-        buffer = io.BytesIO()
-        youtube_audio = YouTube(url).streams.get_audio_only()
-        youtube_audio.stream_to_buffer(buffer)
-        
-        #connect to the VC
-        voice_client = await channel.connect()
-        
-        #seek to buffer to the start just in case
-        buffer.seek(0)
-        
-        #generate the Player instance that will handle cleanup with existing request data
-        player = Player(voice_client, asyncio.get_running_loop())
-        
-        #generate the audio and play it
-        source = discord.FFmpegPCMAudio(buffer, pipe=True)
-        voice_client.play(source, after=player.play_after)# after is the callback to our player cleanup code
-        
-        #modify the original interaction message to show we are live and also refresh the interaction window
-        await interaction.edit_original_response(content=f"You're playing {youtube_audio.title}")
-    else:
-        interaction = await ctx.respond("Sorry, I'm already playing a song somewhere.")
+    if is_connected(ctx):
+        await players[ctx.guild].queue(ctx, url)
+        return 
+    
+    interaction = await ctx.respond("Loading this bitch up")
+    channel = ctx.author.voice.channel
+    
+    #connect to the VC
+    voice_client = await channel.connect()
+    
+    #generate the Player instance that will handle cleanup with existing request data
+    players[ctx.guild] = Player(voice_client, asyncio.get_running_loop())
+    await players[ctx.guild].play(url, ctx, interaction)
 
 #code that checks using the request context if the bot is connected to a Voice Channel in the same server(partial fix to multi-call issue)
 def is_connected(ctx):
--- player.py
+++ player.py
@@ -1,16 +1,53 @@
-import os, asyncio
+import asyncio, discord, io
+from pytube import YouTube
 
-# helper class that stores context and other info relevant to the current playback task to be interacted with after playback is finished
 class Player:
+    """
+        Helper class that provides music playing services
+    """
+
     def __init__(self, voice_client, loop):
-        self.voice_client = voice_client
-        self.loop = loop
+        self._voice_client = voice_client
+        self._loop = loop
+        self._queue = []
+        self._ctx = None
+        self._current_title = ""
+        self._last_interaction = None
+
+    async def play(self, url, ctx=None, loading_interaction = None):
+        buffer = io.BytesIO()
+
+        youtube_audio = YouTube(url).streams.get_audio_only()
+        youtube_audio.stream_to_buffer(buffer)
+        buffer.seek(0)
+        self._current_title = youtube_audio.title
+
+        source = discord.FFmpegPCMAudio(buffer, pipe=True)
+        self._voice_client.play(source, after=self.play_after)
+        
+        if loading_interaction is not None:
+            await loading_interaction.edit_original_response(content=f"Now playing: {self._current_title}!")
+        else:
+            await self._ctx.send(f"Now playing: {self._current_title}!")
+        
+
+    async def queue(self, ctx, url):
+        self._ctx = ctx
+        self._queue.append(url)
+        youtube_audio = YouTube(url).streams.get_audio_only()
+        self._last_interaction = await ctx.respond(f"Track {youtube_audio.title} queued!")
+
+    @property
+    def current_title(self):
+        return self._current_title
 
     def play_after(self, error):
         if error is not None:
             print(error)
+        elif self._queue is not None and self._queue != []:
+            asyncio.run_coroutine_threadsafe(self.play(self._queue.pop(0)), self._loop)
         else:
-            asyncio.run_coroutine_threadsafe(self.disconnect(), self.loop)
+            asyncio.run_coroutine_threadsafe(self.disconnect(), self._loop)
 
     async def disconnect(self):
-        await self.voice_client.disconnect()
+        await self._voice_client.disconnect()
