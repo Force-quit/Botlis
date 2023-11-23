c87365ab20cde7573f0a10ce2e3b457e66d5357b
Force-quit <miloulune1@gmail.com>
2023-11-22 20:35:40 -0500

    It works!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from dotenv import dotenv_values
+from pytube import YouTube
 
 config = dotenv_values(".env")
 
@@ -11,5 +12,16 @@ async def ping(ctx):
     message = await interraction.original_response()
     await message.add_reaction("👌")
 
+@bot.slash_command()
+async def play(ctx, url):
+    interaction = await ctx.respond("Loading this bitch up")
+    channel = ctx.author.voice.channel
+    youtube_audio = YouTube(url).streams.filter(only_audio=True).first()
+    audio_file = youtube_audio.download(output_path="downloads")
+    voice_client = await channel.connect()
+    source = discord.FFmpegPCMAudio(audio_file)
+    voice_client.play(source)
+    await interaction.edit_original_response(content=f"You're playing {youtube_audio.title}")
+    
 if __name__ == "__main__":
     bot.run(config["TOKEN"])
